a47055342c9783b80243a95858215aa2e6d42c85
S Vaibhav <s.vaibhav15656@gmail.com>
2026-02-27 08:49:23 +0530

    updated training script and predictiction script

--- train_model.py
+++ train_model.py
@@ -2,30 +2,27 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
 #load dataset
-df = pd.read_csv("commit_data.csv")
-#create label
-df["risky"] = ((df["insertions"]+df["deletions"])>100).astype(int)
+df=pd.read_csv("commit_data.csv")
+#create features
+df["risky"]=((df["insertions"]+df["deletions"])>100).astype(int)
 #select features
 X = df[["msg_len","files_changed"]]
-y = df["risky"]
+y=df["risky"]
 #split data
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 #create model
-model = RandomForestClassifier(n_estimators=100,random_state=42)
-#train
-model.fit(X_train,y_train)
+model = RandomForestClassifier(n_estimators=100,random_state=42)   # trained only on the train data
+model.fit(X_train,y_train)                                         # trained only on the train data
 #evaluate
-y_pred= model.predict(X_test)
-accuracy = accuracy_score(y_pred,y_test)
-print("model_accuracy",accuracy)
-
-print("\nconfusion matrix")
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test,y_pred)
+print("accuracy score of the model is",accuracy)
+print("confusion matrix")
 print(confusion_matrix(y_test,y_pred))
-print("\nclassification report:")
-print(classification_report(y_pred,y_test))
-
-#save model
+print("classification report")
+print(classification_report(y_test,y_pred))
 joblib.dump(model,"risk_model.pkl")
-print("\n model saved as risk_model.pkl")
+print("model saved as risk_model.pkl")
+
